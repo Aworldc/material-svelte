0fed64c362481c3e8d37925b5ce65e9c9e7f4f0b
Aworldc <83331712+Aworldc@users.noreply.github.com>
2023-08-07 18:04:11 +1000

    Add files via upload

---
+++ components/MdCheckbox.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import '@material/web/checkbox/checkbox.js'
+
+    export let label: string | undefined = undefined
+    export let labelled: boolean = false
+
+    export let change: Function = ()=>{}
+
+    export let checked: boolean = false
+    export let indeterminate = false
+    export let name: string | undefined = undefined
+
+    let id = `${Math.random().toString()}${Math.random().toString()}${Math.random().toString()}`
+
+    let elem
+
+    function update() {
+        if (elem) {
+            checked = elem.checked
+            indeterminate = elem.indeterminate
+        }
+        change()
+    }
+</script>
+
+{#if labelled}
+    <span style="display: inline-flex; align-items: center;">
+        <md-checkbox {id} {checked} {indeterminate} {name} bind:this={elem} on:change={update}></md-checkbox>
+        <label style="font-family: 'Roboto', sans-serif; user-select: none;" for={id}>{label}</label>
+    </span>
+{:else}
+    <md-checkbox {checked} {indeterminate} {name} bind:this={elem} on:change={update}></md-checkbox>
+{/if}
---
+++ index.js
@@ -0,0 +1,15 @@
+import MdApp from './components/MdApp.svelte'
+import MdButton from './components/MdButton.svelte'
+import MdCheckbox from './components/MdCheckbox.svelte'
+import MdFab from './components/MdFab.svelte'
+import MdFlex from './components/MdFlex.svelte'
+import MdProgress from './components/MdProgress.svelte'
+
+export {
+    MdApp,
+    MdButton,
+    MdCheckbox,
+    MdFab,
+    MdFlex,
+    MdProgress
+}
